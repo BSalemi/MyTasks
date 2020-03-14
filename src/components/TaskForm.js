0b65c91e53f7596fa73eac1f93e81ebec5038e84
Brian <briandsalemi@gmail.com>
2020-03-14 16:48:53 -0400

    created TaskForm component and returning a form

---
+++ src/components/TaskForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TaskForm extends React.Component{
+
+    render(){
+        return(
+            <div id="task-form">
+                <form>
+                    <input></input>
+                </form>
+            </div>
+        )
+    }
+
+}
