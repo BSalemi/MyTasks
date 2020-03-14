6220a3a163e5a78b1b76bcccc7c1214b9d6c31ca
Brian <briandsalemi@gmail.com>
2020-03-14 19:35:42 -0400

    created and exported new Logout component which will be responsible for rendering the Logout button

---
+++ src/components/Logout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Logout extends React.Component{
+    render(){
+        return(
+            <div id="logout">
+
+            </div>
+        )
+    }
+}
+
+export default Logout
