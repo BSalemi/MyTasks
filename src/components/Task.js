3d2011f3a5a87b005bbcc9250e7c759b8c975f6f
Brian <briandsalemi@gmail.com>
2020-03-14 17:33:46 -0400

    created and exported new Task component to handle individual tasks

---
+++ src/components/Task.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Task extends React.Component{
+
+    render(){
+        return(
+            <div id="task">
+
+            </div>
+        )
+    }
+}
+
+export default Task
