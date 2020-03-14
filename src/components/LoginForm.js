f158f87cf582fb74a46a88984764fff887f301c1
Brian <briandsalemi@gmail.com>
2020-03-14 17:37:27 -0400

    created and exported new component LoginForm to handle logging in

---
+++ src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginForm extends React.Component{
+    render(){
+        return(
+            <div id="login-form">
+
+            </div>
+        )
+    }
+}
+
+export default LoginForm 
