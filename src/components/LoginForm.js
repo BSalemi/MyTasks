cef224b13662fb236891df4e465929c101e71780
Brian <briandsalemi@gmail.com>
2020-03-18 18:29:14 -0400

    added input fields for username and password and submit button to LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,12 @@ class LoginForm extends React.Component{
     render(){
         return(
             <div id="login-form">
-
+                <form>
+                    <input type="text" name="Enter your Username"/>
+                    <br/>
+                    <input type="password" name="Enter your Password"/>
+                    <input type="submit" value="Login"/>
+                </form>
             </div>
         )
     }
