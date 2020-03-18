d561e1a985c5f4bde2c347a17c1eb9500d9f411e
Brian <briandsalemi@gmail.com>
2020-03-18 18:58:00 -0400

    added state for task to TaskForm component added onChange event and handleOnChange function that updates the input field and state with each key entered

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 class TaskForm extends React.Component{
 
+    state = {
+        task: ""
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            task: event.target.value
+        })
+    }
+
     render(){
         return(
             <div id="task-form">
                 <form>
-                    <input type="text" name="What do you have to do?"/>
+                    <input type="text" name="What do you have to do?" value={this.state.task} onChange={event => this.handleOnChange(event)}/>
                     <input type="submit" value="Add to List"/>
                 </form>
             </div>
