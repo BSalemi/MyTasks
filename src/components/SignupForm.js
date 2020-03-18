ba3977f64e6a07475e7a684de0df04673a3a0550
Brian <briandsalemi@gmail.com>
2020-03-18 16:04:29 -0400

    created a Sign Up form for SignupForm component. Included input fields for email address, username, and password

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -4,7 +4,15 @@ class SignupForm extends React.Component {
     render(){
         return(
             <div id="signup-form">
-
+                <form>
+                    <input type="text" name="Enter your Email Address"/>
+                    <br/>
+                    <input type="text" name="Select a Username"/>
+                    <br/>
+                    <input type="password" name="Enter a Password"/>
+                    <br/>
+                    <input type="submit" value="Sign Up"/>
+                </form>
             </div>
         )
     }
