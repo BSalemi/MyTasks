3af453a01f905d4eb1d614f34dad82c55838f9b6
Brian <briandsalemi@gmail.com>
2020-03-31 12:02:41 -0400

    add onChange event handler to each input field in SignupForm component. The event handler calls handleOnChange function which updates state with each  character that's  entered into an input field

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -8,18 +8,24 @@ class SignupForm extends React.Component {
         password: "",
         passwordConfirm: ""
     }
+
+    handleOnChange =(event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     
     render(){
         return(
             <div id="signup-form">
                 <form>
-                    <input type="text" name="emailAddress" placeholder="Enter your Email Address"/>
+                    <input type="text" name="emailAddress" placeholder="Enter your Email Address" value={this.state.emailAddress} onChange={event => this.handleOnChange(event)}/>
                     <br/>
-                    <input type="text" name="username" placeholder="Choose a Username"/>
+                    <input type="text" name="username" placeholder="Choose a Username" value={this.state.username} onChange={event => this.handleOnChange(event)}/>
                     <br/>
-                    <input type="password" name="password" placeholder="Enter a Password"/>
+                    <input type="password" name="password" placeholder="Enter a Password" value={this.state.password} onChange={event => this.handleOnChange(event)}/>
                     <br/>
-                    <input type="password" name="passwordConfirm" placeholder="Confirm Password"/>
+                    <input type="password" name="passwordConfirm" placeholder="Confirm Password" value={this.state.passwordConfirm} onChange={event => this.handleOnChange(event)}/>
                     <br/>
                     <input type="submit" value="Sign Up"/>
                 </form>
