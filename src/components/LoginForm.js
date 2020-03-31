d6eaf5b81d2f9a43f65094a93451b7b7f754d147
Brian <briandsalemi@gmail.com>
2020-03-31 12:20:07 -0400

    added state to LoginForm component. added value attributes to input field and onChange event listener to each input. Event listener calls handleOnChange function to update state with each character entered

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 class LoginForm extends React.Component{
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleOnChange = (event) =>{
+        this.setState({
+            [event.target.name]: event.target.value 
+        })
+    }
+
     render(){
         return(
             <div id="login-form">
                 <form>
-                    <input type="text" name="username" placeholder="Enter your Username"/>
+                    <input type="text" name="username" placeholder="Enter your Username" value={this.state.username} onChange={event => this.handleOnChange(event)}/>
                     <br/>
-                    <input type="password" name="password" placeholder="Enter your Password"/>
+                    <input type="password" name="password" placeholder="Enter your Password" value={this.state.password} onChange={event => this.handleOnChange(event)}/>
                     <input type="submit" value="Login"/>
                 </form>
             </div>
