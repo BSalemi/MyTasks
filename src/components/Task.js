f70930161fad52cc897c914c6e0526b0e2b72742
Brian <briandsalemi@gmail.com>
2020-05-16 13:56:06 -0400

    uncommented generateTasks function in Tasks component. Added return tasksCards to generateTasks function and rendered the function in Tasks component. Destructured props in Task component and added some CSS styling to the task cards

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,8 +3,9 @@ import React from 'react';
 class Task extends React.Component{
 
     render(){
+        const {id, to_do, completed} = this.props;
         return(
-            <div id="task">
+            <div id="task" className="tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5">
 
             </div>
         )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,16 +5,17 @@ import Task from './Task.js'
 class Tasks extends React.Component{
 
 
-    // generateTasks = () => {
-    //     const tasksCards = this.props.tasks.map(task => {
-    //         return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed}/>
-    //     })
-    // }
+    generateTasks = () => {
+        const tasksCards = this.props.tasks.map(task => {
+            return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed}/>
+        })
+        return tasksCards
+    }
 
     render(){
         return(
             <div id="tasks">
-
+                {this.generateTasks()}
             </div>
         )
     }
