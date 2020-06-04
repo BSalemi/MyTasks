a1ce8a5d2b95ee6ad2cebac774b3c7c0675d5f68
Brian <briandsalemi@gmail.com>
2020-06-04 13:17:55 -0400

    added a constructor to Clock component and state to keep track of time. Also added componentDidMount with a setInterval function to call currentTime

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TasksContainer from './containers/TasksContainer.js'
 import UserContainer from './containers/UserContainer.js'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
+import Clock from './components/Clock'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
@@ -25,6 +26,7 @@ class App extends React.Component{
     return(
       <div id="app" className="tc">
         <h1 className="f1"> My Tasks</h1>
+          <Clock/>
           <TasksContainer currentUser={this.state.loggedInUser} />
           <UserContainer updateLoggedInUser={this.updateLoggedInUser}/>
           <Route exact path="/login" component={LoginForm} />
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,23 +2,36 @@ import React from 'react';
 
 class Clock extends React.Component {
 
+
+    componentDidMount() {
+        setInterval(()=> {
+            this.currentTime();
+        })
+    }
+    
+    constructor() {
+        super();
+        this.state = {
+            time: "00:00:00 AM"
+        }
+    }
+    
+
     currentTime = () => {
         let date = new Date(),
             hour = date.getHours(),
             min = date.getMinutes(),
             sec = date.getSeconds(),
-            midday = "AM",
+            midday = "AM";
 
         hour = this.updateTime(hour);
         min = this.updateTime(min);
         sec = this.updateTime(sec);
-
-        
+ 
         midday = (hour >= 12) ? "PM" : "AM";
-
-        document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + midday;
-
-        let t = setTimeout(function(){ currentTime() }, 1000);
+        hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
+        let t = setTimeout(function(){ this.currentTime() }, 1000);
+        return hour + " : " + min + " : " + sec +  midday;
     }
 
     updateTime = (k) => {
@@ -31,9 +44,11 @@ class Clock extends React.Component {
 
     render(){
         return(
-            <div id="clock">
+            <div id="clock" ref={this.clockRef}>
                 {this.currentTime()}
             </div>
         )
     }
 }
+
+export default Clock
