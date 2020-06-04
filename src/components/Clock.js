d2d02fb51acce1b050d910f94d1eb40d53b31499
Brian <briandsalemi@gmail.com>
2020-06-04 11:29:40 -0400

    created the updateTime function in Clock component which checks if the current value of hours, minutes, and seconds are less than 10 or not. If they are then a zero will be added before the value.

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,21 +2,28 @@ import React from 'react';
 
 class Clock extends React.Component {
 
- currentTime = () => {
-    let date = new Date();
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
+    currentTime = () => {
+        let date = new Date();
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
 
-    hour = this.updateTime(hour);
-    min = this.updateTime(min);
-    sec = this.updateTime(sec);
+        hour = this.updateTime(hour);
+        min = this.updateTime(min);
+        sec = this.updateTime(sec);
 
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
+        document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
 
-    let t = setTimeout(function(){ currentTime() }, 1000);
+        let t = setTimeout(function(){ currentTime() }, 1000);
+    }
 
-}
+    updateTime = (k) => {
+        if (k < 10) {
+            return "0" + k;
+        } else {
+            return k; 
+        }
+    }
 
     render(){
         return(
