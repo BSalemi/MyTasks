c1094f9178582d0a4ac20f5c8a893df9759f3f69
Brian <briandsalemi@gmail.com>
2020-06-07 16:15:29 -0400

    added a Complete and Undo button to Task component. Using a ternary operator to check if task.completed is true, if so display Complete button else Undo button. Some CSS styling was added to the two buttons too.

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,17 +3,23 @@ import React from 'react';
 class Task extends React.Component{
 
     render(){
-        const {id, toDo, completed} = this.props;
-        console.log(this.props.completed, "task props")
+        const {id, toDo} = this.props;
+        
         return(
+            
             <div id="task" className="tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5">
                
                 <h2>
                     {toDo}
                 </h2>
                 <p>
-                    {{completed} ? "Status - Incomplete" : "Status - Complete"}
+                    {this.props.completed ? "Status - Complete" : "Status - Incomplete"}
                 </p>
+                <div>
+                    {this.props.completed ?  <button className= "tc pa1 ma1 br3 bw2">Undo</button> :  <button className="tc pa1 ma1 br3 bw2 dib">Complete</button>}
+                </div>
+               
+               
 
             </div>
         )
