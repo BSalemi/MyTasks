b4b8c755963e0a4f7df926b0a82ca5fe54f0442a
Brian <briandsalemi@gmail.com>
2020-08-23 13:21:41 -0400

    added hasDueDate to state in TaskForm component. Created variables dateInput and dateDue in handleOnSubmit function in TaskForm to set the value of dueDate in toDo equal to the value of the date input field or an empty string

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -6,7 +6,8 @@ class TaskForm extends React.Component{
 
     state = {
         task: "",
-        dueDate: false
+        hasDueDate: false,
+        dueDate: ""
     }
 
     handleOnChange = (event) => {
@@ -20,31 +21,38 @@ class TaskForm extends React.Component{
         this.setState((prevState) => {
             return {
                 ...this.state,
-                dueDate: !prevState.dueDate
+                hasDueDate: !prevState.hasDueDate
             }
         })
     }
 
     handleOnSubmit = (event) => {
-        const {task} = this.state
+        const {task} = this.state,
+               dateInput = document.getElementById('due-date-input');
+        let dateDue;
+
+        dateInput ? dateDue = dateInput.value : dateDue = ""
+
         event.preventDefault();
+
         if(task.length > 0){
             const toDo = {
                 task: task,
-                user_id: localStorage.loggedIn
+                user_id: localStorage.loggedIn,
+                dueDate: dateDue
             };
-                fetch(TASKS_URL,{ 
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json"
-                    },
-                    body: JSON.stringify(toDo)
-                })
-                .then(res => res.json())
-                .then((newTask) => {
-                    this.props.addTask(newTask);
-                }) 
+            fetch(TASKS_URL,{
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                },
+                body: JSON.stringify(toDo)
+            })
+            .then(res => res.json())
+            .then((newTask) => {
+                this.props.addTask(newTask);
+            })
             this.setState({
                 task: ""
             })
@@ -54,13 +62,14 @@ class TaskForm extends React.Component{
     }
 
     render(){
-       const {dueDate} = this.state
+
+       const {hasDueDate} = this.state
         return(
             <div>
                 <form className="task-form" onSubmit={event => this.handleOnSubmit(event)} >
                     <input type="text" placeholder="What do you have to do?" value={this.state.task} onChange={event => this.handleOnChange(event)}/>
                     <button onClick={event => this.handleOnClick(event)} className="due-date-button">Add Due Date</button>
-                    {!dueDate ? null : <input type="date" className="due-date-input"/>}
+                    {!hasDueDate ? null : <input type="date" id="due-date-input"/>}
                     <input type="submit" value="Add New Task" />
                 </form>
             </div>
