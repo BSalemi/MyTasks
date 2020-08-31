67478e42a70628e02646ba95625fa982a6a2a544
Brian <briandsalemi@gmail.com>
2020-08-31 18:43:08 -0400

    Added tasksWithDates and tasks to Tasks state. Created the sortTasks function which filters tasks props and sets state depending on if there is a due date or not. Created a generateTasksWithDeadlines function and changed generateTasks function to handle the generation of tasks with and without deadlines. Added h3 tags to label the newly sorted tasks in Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,25 +4,53 @@ import Task from './Task.js';
 
 class Tasks extends React.Component{
 
+    state = {
+        tasksWithDates: [],
+        tasks: []
+    }
+
+    componentDidMount(){
+        this.sortTasks()
+    }
+
+    sortTasks = () => {
+
+        const deadlineTasks = this.props.tasks.filter(task => task.due_date !== null);
+        const tasksWithoutDeadline = this.props.tasks.filter(task => task.due_date === null)
+        
+        this.setState({
+            tasksWithDates: deadlineTasks,
+            tasks: tasksWithoutDeadline
+        })
+    }
     generateTasks = () => {
-        // const userTasks = this.props.tasks.filter((task) => {
-        //     return task.user_id === parseInt(this.props.currentUser, 10)})
-        
-        
-        const tasksCards = this.props.tasks.map(task => {
+
+        const tasksCards = this.state.tasks.map(task => {
             return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={this.props.updateTask} deleteTask={this.props.deleteTask} user_id={task.user_id}/>
         })
         return tasksCards
     }
 
-    
+    generateTasksWithDeadlines = () => {
+        const tasksCards = this.state.tasksWithDates.map(task => {
+            return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={this.props.updateTask} deleteTask={this.props.deleteTask} user_id={task.user_id}/>
+        })
+        return tasksCards
+    }
 
 
     render(){
-        const {tasks} = this.props
+        const {tasks, tasksWithDates} = this.state
+        console.log(this.state, "state")
+
         return(
             <div id="tasks">
-                {tasks.length > 0 ? this.generateTasks() : null }
+                {tasksWithDates.length > 0 ? (
+                <><h3>High Priority:</h3> {this.generateTasksWithDeadlines()}</>)
+                 : null }
+                {tasks.length > 0 ? (
+                <><h3>At Your Leisure:</h3>{this.generateTasks()}</>)
+                 : null}
             </div>
         )
     }
