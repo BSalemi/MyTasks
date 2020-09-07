d932e95c03e7e59973639cf95d5b3f6c4cc20c11
Brian <briandsalemi@gmail.com>
2020-09-07 09:22:37 -0400

    Added value attributes and onClick event listeners to the buttons within Filter Component. Created a handleOnClick function which calls props.filterTasks to filter the tasks depending on the event target's value.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class Filter extends React.Component{
+
+    handleOnClick = (e) => {
+        const {tasks, filterTasks} = this.props
+        if(e.target.value === "completed"){
+            const completedTasks = tasks.filter(task => task.completed === true);
+            filterTasks(completedTasks)
+        } 
+        if (e.target.value === "deadlines"){
+            const tasksWithDeadlines = tasks.filter(task => task.due_date !== null && task.completed === false);
+            filterTasks(tasksWithDeadlines)
+        }
+        if (e.target.value === "no-deadlines"){
+            const tasksWithoutDeadlines = tasks.filter(task => task.due_date === null && task.completed === false);
+            filterTasks(tasksWithoutDeadlines)
+        }
+        if(e.target.value === "all"){
+            filterTasks(tasks)
+        }
+    }
     render(){
         return(
             <div className="filterBar">
-                <button>All Tasks</button>
-                <button>Tasks With Deadlines</button>
-                <button>Tasks Without Deadlines</button>
-                <button>Completed Tasks</button>
+                <button onClick={e => this.handleOnClick(e)} value="all">All Tasks</button>
+                <button onClick={e => this.handleOnClick(e)} value="deadlines">Tasks With Deadlines</button>
+                <button onClick={e => this.handleOnClick(e)} value="no-deadlines">Tasks Without Deadlines</button>
+                <button onClick={e => this.handleOnClick(e)} value="completed">Completed Tasks</button>
             </div>
         )
     }
