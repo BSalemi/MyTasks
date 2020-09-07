ac34621d5f2758b9c6bd235aaafbfcd5a70bb6ec
Brian <briandsalemi@gmail.com>
2020-09-07 09:26:42 -0400

    Removed the functions for generating different tasks and instead just using one generateTasks function which iterates over the tasks prop or filteredTasks prop depending on if filteredTasks is an empty array or not

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,50 +6,30 @@ class Tasks extends React.Component{
 
  
     generateTasks = () => {
-        const filteredTasks = this.props.tasks.filter(task => task.due_date === null && task.completed === false)
-        const tasksCards = filteredTasks.map(task => {
-            return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={this.props.updateTask} deleteTask={this.props.deleteTask} user_id={task.user_id}/>
-        })
-        return tasksCards
-    }
+        const {tasks, filteredTasks, updateTask, deleteTask} = this.props
 
-    generateTasksWithDeadlines = () => {
-        const filteredTasks = this.props.tasks.filter(task => task.due_date !== null && task.completed === false)
-        const sortedTasks = filteredTasks.sort((a, b) => Date.parse(a.due_date) - Date.parse(b.due_date))
-        console.log(sortedTasks, "sorted")
-        const tasksCards = sortedTasks.map(task => {
-            return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={this.props.updateTask} deleteTask={this.props.deleteTask} user_id={task.user_id}/>
-        })
-        return tasksCards
-    }
-
-    generateCompletedTasks = () => {
-        const filteredTasks = this.props.tasks.filter(task => task.completed === true)
-        const tasksCards = filteredTasks.map(task => {
-            return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={this.props.updateTask} deleteTask={this.props.deleteTask} user_id={task.user_id}/>
-        })
-        return tasksCards
+        if(filteredTasks.length > 0){
+            const tasksCards = filteredTasks.map(task => {
+                return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={updateTask} deleteTask={deleteTask} user_id={task.user_id}/>
+            })
+            return tasksCards
+        } else {
+            const tasksCards = tasks.map(task => {
+                return <Task key={task.id} id={task.id} toDo={task.to_do} completed={task.completed} dueDate={task.due_date} updateTask={updateTask} deleteTask={deleteTask} user_id={task.user_id}/>
+            })
+            return tasksCards
+        }
     }
 
 
     render(){
         const {tasks} = this.props
-        const tasksWithDates = tasks.filter(task => task.due_date !== null),
-              tasksWithNoDates = tasks.filter(task => task.due_date === null),
-              completedTasks = tasks.filter(task => task.completed === true)
-        
 
         return(
-            <div id="tasks">
-                {tasksWithDates.length > 0 ? (
-                <><h3>High Priority:</h3> {this.generateTasksWithDeadlines()}</>)
-                 : null }
-                {tasksWithNoDates.length > 0 ? (
-                <><h3>At Your Leisure:</h3>{this.generateTasks()}</>)
-                 : null}
-                 {completedTasks.length > 0 ? (
-                <><h3>Completed:</h3>{this.generateCompletedTasks()}</>)
-                 : null}
+            <div className="tasks">
+                {tasks.length > 0 ? (
+                    this.generateTasks()
+                ) : null }
             </div>
         )
     }
